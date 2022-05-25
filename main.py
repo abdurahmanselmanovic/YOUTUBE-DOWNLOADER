5908c416935ba176cdf4c98f5fd67ab6ee76df49
Abdurahman -Git <selmanovicabdurrahman@gmail.com>
2022-05-25 17:02:34 +0200

    commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pytube import YouTube
 
+
 link = input("Enter the link of YouTube video you want to download:  ")
 yt = YouTube(link)
 
@@ -10,3 +11,7 @@ print("Length of video: ", yt.length)
 print("Rating of video: ", yt.rating)
 
 ys = yt.streams.get_highest_resolution()
+
+
+# IF PROGRAM DONT WORK YOU NEED TO INSTALL PYTUBE
+# pip install pytube
