515aa1d2fa3e36399117b9d4455df1150308aab1
Abdurahman -Git <selmanovicabdurrahman@gmail.com>
2022-05-25 16:47:50 +0200

    newcommit

--- main.py
+++ main.py
@@ -1,29 +1,16 @@
-from cProfile import label
-from cgitb import text
-import imp
-import string
-import tkinter as tk
-from tkinter import *
-from tkinter.tix import COLUMN
 from pytube import YouTube
-from tkinter import filedialog, messagebox
+import os
 
 
-def createWidgets():
-    link_label = label(root, text="Youtube URL:  ", bg="#E8D579")
-    link_label.grid(row=1, column=0, pady=5, padx=5)
-    root.link_text = Entry(root, width=60, textvariable=video_link)
-    root.link_text.grid(row=1, column=1, pady=5, padx=5)
+def downloadYouTube(videourl, path):
+
+    yt = YouTube(videourl)
+    yt = yt.streams.filter(progressive=True, file_extension='mp4').order_by(
+        'resolution').desc().first()
+    if not os.path.exists(path):
+        os.makedirs(path)
+    yt.download(path)
 
 
-root = tk.Tk()
-
-root.geometry('600x120')
-root.resizable(False, False)
-root.title("Youtube Downloader")
-root.config(background="#000000")
-video_link = StringVar()
-
-createWidgets()
-
-root.mainloop()
+downloadYouTube('https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+                './videos/FindingNemo1')
