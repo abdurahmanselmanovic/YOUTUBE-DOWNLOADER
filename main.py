03a3b73e846118ef0fad51cb1eb1f4f97c2d4915
Abdurahman -Git <selmanovicabdurrahman@gmail.com>
2022-05-25 16:41:50 +0200

    commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from cProfile import label
 from cgitb import text
 import imp
+import string
 import tkinter as tk
 from tkinter import *
 from tkinter.tix import COLUMN
@@ -10,7 +11,9 @@ from tkinter import filedialog, messagebox
 
 def createWidgets():
     link_label = label(root, text="Youtube URL:  ", bg="#E8D579")
-    link_label.grid(row=1, column=1, pady=5, padx=5)
+    link_label.grid(row=1, column=0, pady=5, padx=5)
+    root.link_text = Entry(root, width=60, textvariable=video_link)
+    root.link_text.grid(row=1, column=1)
 
 
 root = tk.Tk()
@@ -19,6 +22,7 @@ root.geometry('600x120')
 root.resizable(False, False)
 root.title("Youtube Downloader")
 root.config(background="#000000")
+video_link = StringVar()
 
 createWidgets()
 
